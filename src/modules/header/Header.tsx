c98c9d6119862e486497970bc013bb57edd9efde
YarnyanDev <92979595+Yarnyan@users.noreply.github.com>
2024-04-11 22:03:22 +0500

    Add files via upload

--- src/modules/header/Header.tsx
+++ src/modules/header/Header.tsx
@@ -1,26 +1,34 @@
-import { AppBar, Toolbar, Typography } from '@mui/material'
-
-
+import styles from './Header.module.scss'
+import { Link } from 'react-router-dom'
+import UserAvatar from '../../components/ui/UserAvatar'
 export const Header = () => {
   return (
-    <AppBar
-      component='header'
-      sx={{
-        height: '80px',
-        position: 'relative'
-      }}
-    >
-      <Toolbar>
-        <Typography
-          component='span'
-          fontSize={32}
-          sx={{
-            margin: 'auto',
-          }}
-        >
-          Header
-        </Typography>
-      </Toolbar>
-    </AppBar>
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <div className={styles.subtitle}>
+          <h1>EdevHub</h1>
+        </div>
+        <div className={styles.navigate}>
+          <div className={styles.navigateItem}>
+            <img src="/icons/repo.png" alt="" />
+            <Link to={'/'}>Репозитории</Link>
+          </div>
+          <div className={styles.navigateItem}>
+            <img src="/icons/edit.png" alt="" />
+            <Link to={'/'}>Конструктор резюме</Link>
+          </div>
+          <div className={styles.navigateItem}>
+            <img src="/icons/message.png" alt="" />
+            <Link to={'/'}>Мессенджер</Link>
+          </div>
+        </div>
+        <div className={styles.tools}>
+          <Link to={'/'} className={styles.toolLink}>
+            <img src="/icons/setting.png" alt="" className={styles.toolIcon} />
+          </Link>
+          <UserAvatar />
+        </div>
+      </div>
+    </header>
   )
 }
