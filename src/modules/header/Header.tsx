5fc764507374a4fc9392107cc4de984d4921b442
YarnyanDev <92979595+Yarnyan@users.noreply.github.com>
2024-04-11 22:00:05 +0500

    Add files via upload

--- src/modules/header/Header.tsx
+++ src/modules/header/Header.tsx
@@ -1,10 +1,34 @@
-import React from 'react'
 import styles from './Header.module.scss'
-
-const Header = () => {
+import { Link } from 'react-router-dom'
+import UserAvatar from '../../components/ui/UserAvatar'
+export const Header = () => {
   return (
-    <div>Header</div>
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <div className={styles.subtitle}>
+          <h1>EdevHub</h1>
+        </div>
+        <div className={styles.navigate}>
+          <div className={styles.navigateItem}>
+            <img src="/icons/repo.png" alt="" />
+            <Link to={'/'}>Репозитории</Link>
+          </div>
+          <div className={styles.navigateItem}>
+            <img src="/icons/edit.png" alt="" />
+            <Link to={'/'}>Конструктор резюме</Link>
+          </div>
+          <div className={styles.navigateItem}>
+            <img src="/icons/message.png" alt="" />
+            <Link to={'/'}>Мессенджер</Link>
+          </div>
+        </div>
+        <div className={styles.tools}>
+          <Link to={'/'} className={styles.toolLink}>
+            <img src="/icons/setting.png" alt="" className={styles.toolIcon} />
+          </Link>
+          <UserAvatar />
+        </div>
+      </div>
+    </header>
   )
 }
-
-export default Header
