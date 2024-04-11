2d13b6eb716796c77478877f4a2c4b9ef9f3216d
YarnyanDev <92979595+Yarnyan@users.noreply.github.com>
2024-04-11 22:00:48 +0500

    Add files via upload

---
+++ src/components/ui/UserAvatar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Avatar from '@mui/material/Avatar';
+type Props = {}
+
+const UserAvatar = (props: Props) => {
+  return (
+    <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+  )
+}
+
+export default UserAvatar
