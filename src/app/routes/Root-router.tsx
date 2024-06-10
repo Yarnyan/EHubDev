d699fb2370ed2b2b7757d50f104291fd839ee78e
YarnyanDev <92979595+Yarnyan@users.noreply.github.com>
2024-06-10 23:37:47 +0500

    Add files via upload

--- src/app/routes/Root-router.tsx
+++ src/app/routes/Root-router.tsx
@@ -1,9 +1,10 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { Layout } from '../layout/Layout.tsx'
-import { AUTHORIZATION_ROUTE, PROFILE_ROUTE, REGISTRATION_ROUTE, RESUMEBUILDER_ROUTE } from '../../consts/routes.ts'
+import { AUTHORIZATION_ROUTE, PROFILE_ROUTE, REGISTRATION_ROUTE, RESUMEBUILDER_ROUTE, SEARCH_ROUTE } from '../../consts/routes.ts'
 import { AuthorizationPage } from '../../pages/Authorization-page.tsx'
 import { ProfilePage } from '../../pages/Profile-page.tsx'
 import { BuilderPage } from '../../pages/Builder-page.tsx'
+import { SearchPage } from '../../pages/search-page.tsx'
 import { ProtectedRoute } from './Protected-route.tsx'
 
 export const RootRouter = () => {
@@ -16,6 +17,9 @@ export const RootRouter = () => {
         </ProtectedRoute>} />
         <Route path={RESUMEBUILDER_ROUTE} element={<ProtectedRoute>
           <BuilderPage />
+        </ProtectedRoute>} />
+        <Route path={SEARCH_ROUTE} element={<ProtectedRoute>
+          <SearchPage />
         </ProtectedRoute>} />
         <Route path={REGISTRATION_ROUTE} element={<AuthorizationPage />} />
         <Route path={AUTHORIZATION_ROUTE} element={<AuthorizationPage />} />
