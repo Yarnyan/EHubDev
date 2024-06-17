9aaa8f5e43c59c54a24b77962006d4dcab52563e
Appdruha <121567209+Appdruha@users.noreply.github.com>
2024-06-17 23:50:52 +0500

    avatar

--- src/components/user-avatar/User-avatar.tsx
+++ src/components/user-avatar/User-avatar.tsx
@@ -1,16 +1,15 @@
-import Avatar from '@mui/material/Avatar';
+import Avatar from '@mui/material/Avatar'
 import { useNavigate } from 'react-router-dom'
 
-interface Avatar {
-  avatar: string | undefined;
-}
-const UserAvatar: React.FC<Avatar> = ({avatar}) => {
+const UserAvatar = () => {
+  const navigate = useNavigate()
 
   return (
     <Avatar
-      sx={{cursor: 'pointer'}}
-      alt="Remy Sharp"
-      src={avatar} />
+      onClick={() => navigate('/profile')}
+      sx={{ cursor: 'pointer' }}
+      alt='Remy Sharp'
+      src='/static/images/avatar/1.jpg' />
   )
 }
 
